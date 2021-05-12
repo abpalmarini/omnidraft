9c74f0a3f11d02b2fcea613ae8fd8a780f4bc807
AlessandroPalmarini <alessandro.palmarini1@gmail.com>
2021-05-12 11:30:50 +0100

    big refactor of updating open roles :)

--- autodraft/selfplay.py
+++ autodraft/selfplay.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 A_PICK = 1
 A_BAN  = 2
 B_PICK = 3
@@ -45,7 +47,6 @@ class Draft:
 
     def apply(self, action):
         to_select = self.to_select()
-        # @Speed can adjust to not update open roles if its last pick
         if to_select == A_PICK:
             self._update_open_roles(action, self.team_A_roles)
         elif to_select == B_PICK:
@@ -74,75 +75,31 @@ class Draft:
             return [c for c, roles in enumerate(self.champs_roles)
                     if available(c) and bool(roles)] 
 
+    # To ensure valid actions only contain champs who can fill an open
+    # role we maintain a set of open roles and a list of roles
+    # partially open (when a selected champ can play more than one).
+    # By checking if the number of unique roles, in some subset of the
+    # partial roles, is equal to the number of champs who can play them
+    # we can determine when they are no longer open.
     def _update_open_roles(self, champ, team_roles):
         options = self.champs_roles[champ].intersection(team_roles['open'])
-        num_partial = len(team_roles['partial'])
-
-        def handle_options():
-            if len(options) == 1:
-                team_roles['open'] -= options
-                for roles in team_roles['partial']:
-                    roles -= options
-            else:
-                team_roles['partial'].append(options)
-
-        def total_resolve(champs_involved):
-            combined = options.union(*team_roles['partial'])
-            if len(combined) == champs_involved:
-                team_roles['open'] -= combined
-                team_roles['partial'] = []
-                return True
-            return False
-
-        def pair_resolve():
-            for i in range(num_partial):
-                combined = options.union(team_roles['partial'][i])
-                if len(combined) == 2:
-                    team_roles['partial'].pop(i)
-                    team_roles['open'] -= combined
+        for n_champs in range(len(team_roles['partial']), 0, -1):
+            for partial_subset in combinations(team_roles['partial'], n_champs):
+                unique_roles = options.union(*partial_subset)
+                if len(unique_roles) == n_champs + 1:
+                    for roles in partial_subset:
+                        team_roles['partial'].remove(roles)
                     for roles in team_roles['partial']:
-                        roles -= combined
-                    return True
-            return False
-
-        def triplet_resolve():
-            for i in range(2):
-                for j in range(1, 3):
-                    if i == j: continue 
-                    pair = team_roles['partial'][i], team_roles['partial'][j]
-                    combined = options.union(*pair)
-                    if len(combined) == 3:
-                        # Pop j before i as j is always larger.
-                        team_roles['partial'].pop(j)
-                        team_roles['partial'].pop(i)
-                        team_roles['open'] -= combined
-                        team_roles['partial'][0] -= combined
-                        return True
-            return False
-
-        if num_partial == 0:
-            handle_options()
-        elif num_partial == 1:
-            if total_resolve(2):
-                pass
-            else:
-                handle_options()
-        elif num_partial == 2:
-            if total_resolve(3):
-                pass
-            elif pair_resolve():
-                pass
-            else:
-                handle_options()
-        elif num_partial == 3:
-            if total_resolve(4):
-                pass
-            elif triplet_resolve():
-                pass
-            elif pair_resolve():
-                pass
-            else:
-                handle_options()
+                        roles -= unique_roles
+                    team_roles['open'] -= unique_roles
+                    return
+        # New champ role(s) was unable to resolve with anything else.
+        if len(options) == 1:
+            team_roles['open'] -= options
+            for roles in team_roles['partial']:
+                roles -= options
+        else:
+            team_roles['partial'].append(options)
 
     def _generate_rewards(self):
         pass
