9d474fc8065f2e25ac5339065357c7ab47d68446
AlessandroPalmarini <alessandro.palmarini1@gmail.com>
2021-06-02 11:16:52 +0100

    tests for making nn input correctly

--- test/test_draft.py
+++ test/test_draft.py
@@ -7,6 +7,7 @@ from autodraft.draft import ComboReward as CR
 def open_roles(draft):
     return draft.roles['A']['open']
 
+
 # Help to quickly create list of role rewards.
 def rrs(champ, roles):
     role_rewards = []
@@ -314,7 +315,204 @@ class TestMiscDraftMethods(unittest.TestCase):
         self.assertEqual(open_roles(draft), set(range(5)))
         self.assertEqual(draft_clone.history, [-1, -1, -1, -1, 0])
         self.assertEqual(open_roles(draft_clone), set(range(4)))
-        
+
+
+class TestNNInput(unittest.TestCase):
+
+    def check_active_features(self, active_features, vector):
+        for feature, value in enumerate(vector):
+            if feature in active_features:
+                self.assertEqual(value, 1, msg=f'feature {feature}')
+            else:
+                self.assertEqual(value, 0, msg=f'feature {feature}')
+
+    def test_draft_state_first_ban(self):
+        draft = Draft()
+        draft_state = draft._make_nn_draft_state_input(0)
+        active_features = set() # The following indices should be 1.
+        active_features.add(0)
+        active_features.add(3)
+        # All roles should be open for both teams.
+        open_roles = set(range(3 + len(draft.format), 3 + len(draft.format) + 10))
+        active_features = active_features.union(open_roles)
+        self.check_active_features(active_features, draft_state)
+        correct_len = 3 + len(draft.format) + 10 + (4 * draft.num_champs)
+        self.assertEqual(len(draft_state), correct_len)
+
+    def test_draft_state_B_first_pick(self):
+        rewards = {'role': [], 'combo': []}
+        rewards['role'] = [RR(0, 0, 0, 0), RR(1, 0, 0, 0), RR(2, 0, 0, 0),
+                           RR(3, 0, 0, 0), RR(4, 0, 0, 0)
+                           ]
+        draft = Draft(rewards=rewards)
+        for i in range(5):
+            draft.apply(i)
+        # It is now B to pick and A should not have role 0 open.
+        draft_state = draft._make_nn_draft_state_input(len(draft.history))
+        active_features = set()
+        offset = 0
+                               # Skipping A to pick.
+        active_features.add(1) # Its a pick.
+        active_features.add(2) # B also has next pick.
+        offset += 3
+        active_features.add(offset + 5)
+        offset += len(draft.format)
+        # B still has all roles open
+        active_features = active_features.union(set(range(offset, offset + 5)))
+        offset += 5
+        # A no longer has 0
+        active_features = active_features.union(set(range(offset + 1, offset + 5)))
+        offset += 5
+        # B has no picks.
+        offset += draft.num_champs
+        # B bans.
+        active_features.add(offset + 1)
+        active_features.add(offset + 3)
+        offset += draft.num_champs
+        # A picks.
+        active_features.add(offset + 4)
+        offset += draft.num_champs
+        # A bans.
+        active_features.add(offset + 0)
+        active_features.add(offset + 2)
+        self.check_active_features(active_features, draft_state)
+
+    def test_draft_state_A_pick(self):
+        rewards = {'role': [], 'combo': []}
+        rewards['role'] = [RR(0, 0, 0, 0), RR(1, 0, 0, 0), RR(2, 0, 0, 0),
+                           RR(3, 0, 0, 0), RR(4, 0, 0, 0), RR(5, 0, 0, 0),
+                           RR(6, 1, 0, 0), RR(7, 1, 0, 0)
+                           ]
+        draft = Draft(rewards=rewards)
+        for i in range(8):
+            draft.apply(i)
+        # It is now A to pick. Both no longer have roles 0 and 1 open.
+        # A doesn't have next pick.
+        draft_state = draft._make_nn_draft_state_input(len(draft.history))
+        active_features = set()
+        offset = 0
+        active_features.add(0) # A to select.
+        active_features.add(1) # Its a pick.
+                               # Skipping next turn.
+        offset += 3
+        active_features.add(offset + 8)
+        offset += len(draft.format)
+        A_open = set(range(offset + 2, offset + 5))
+        offset += 5
+        B_open = set(range(offset + 2, offset + 5))
+        offset += 5
+        active_features = active_features.union(A_open, B_open)
+        # A picks.
+        active_features.add(offset + 4)
+        active_features.add(offset + 7)
+        offset += draft.num_champs
+        # A bans.
+        active_features.add(offset + 0)
+        active_features.add(offset + 2)
+        offset += draft.num_champs
+        # B picks.
+        active_features.add(offset + 5)
+        active_features.add(offset + 6)
+        offset += draft.num_champs
+        # B bans.
+        active_features.add(offset + 1)
+        active_features.add(offset + 3)
+        self.check_active_features(active_features, draft_state)
+
+    def test_draft_state_in_make_nn_input(self):
+        # Copying and pasting from :test_draft_state_first_ban as I
+        # just need to test its returned correctly in outer method.
+        draft = Draft()
+        draft_state, _ = draft.make_nn_input()
+        active_features = set()
+        active_features.add(0)
+        active_features.add(3)
+        open_roles = set(range(3 + len(draft.format), 3 + len(draft.format) + 10))
+        active_features = active_features.union(open_roles)
+        self.check_active_features(active_features, draft_state)
+
+    def test_role_reward(self):
+        rewards = {'role': [], 'combo': []}
+        rewards['role'] = [RR(53, 4, 0.2, 0.1)]
+        draft = Draft(rewards=rewards)
+        _, nn_rewards = draft.make_nn_input()
+        reward = nn_rewards[0]
+        # It is A to ban so first value should be A's.
+        self.assertEqual(reward[0], 0.2)
+        self.assertEqual(reward[1], 0.1)
+        active_features = set()
+        active_features.add(2) # Role reward indicator.
+        offset = 3
+        active_features.add(offset + 4)
+        offset += 5
+        active_features.add(offset + 53)
+        # Masking reward values to check other features now we have
+        # asserted they are correct.
+        reward[0] = 0
+        reward[1] = 0
+        self.check_active_features(active_features, reward)
+        # Now checking values when it is B to select.
+        draft.apply(53)
+        _, nn_rewards = draft.make_nn_input()
+        reward = nn_rewards[0]
+        self.assertEqual(reward[0], 0.1)
+        self.assertEqual(reward[1], 0.2)
+        reward[0] = 0
+        reward[1] = 0
+        self.check_active_features(active_features, reward)
+
+    def test_combo_reward(self):
+        rewards = {'role': [RR(0, 0, 0, 0)], 'combo': []}
+        rewards['combo'] = [CR({0, 9}, {17, 22, 41}, 0.5, 0.9)]
+        draft = Draft(rewards=rewards)
+        _, nn_rewards = draft.make_nn_input()
+        # Reward 0 will be the role reward (so I can ban a champ).
+        reward = nn_rewards[1]
+        self.assertEqual(reward[0], 0.5)
+        self.assertEqual(reward[1], 0.9)
+        active_features = set()
+        offset = 3 + 5
+        active_features.add(offset + 0)
+        active_features.add(offset + 9)
+        offset += draft.num_champs
+        active_features.add(offset + 17)
+        active_features.add(offset + 22)
+        active_features.add(offset + 41)
+        reward[0] = 0
+        reward[1] = 0
+        self.check_active_features(active_features, reward)
+        # Checking when B to select.
+        draft.apply(0)
+        _, nn_rewards = draft.make_nn_input()
+        reward = nn_rewards[1]
+        self.assertEqual(reward[0], 0.9)
+        self.assertEqual(reward[1], 0.5)
+        reward[0] = 0
+        reward[1] = 0
+        self.check_active_features(active_features, reward)
+
+    def test_multiple_rewards(self):
+        rewards = {'role': [RR(0, 0, 0.5, 0.8)],
+                   'combo': [CR({2, 8}, set(), 0.9, 0.1)]
+                   }
+        # Setting history up to A's first pick.
+        history = [-1 for _ in range(4)]
+        open_roles_history = [(tuple(range(5)), tuple(range(5))) for _ in range(5)]
+        draft = Draft(history=history, rewards=rewards)
+        draft.roles['open_history'] = open_roles_history
+        _, nn_rewards = draft.make_nn_input()
+        self.assertEqual(nn_rewards[0, 0], 0.5)
+        self.assertEqual(nn_rewards[0, 1], 0.8)
+        self.assertEqual(nn_rewards[1, 0], 0.9)
+        self.assertEqual(nn_rewards[1, 1], 0.1)
+        nn_rewards[:, 0] = 0
+        nn_rewards[:, 1] = 0
+        role_active_features = {2, 3, 8} # Role indicator, role 0, champ 0
+        offset = 3 + 5
+        combo_active_features = {offset + 2, offset + 8}
+        self.check_active_features(role_active_features, nn_rewards[0])
+        self.check_active_features(combo_active_features, nn_rewards[1])
+
 
 if __name__ == '__main__':
     unittest.main()
