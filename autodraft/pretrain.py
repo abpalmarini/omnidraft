collate fn for a batch of pretraing examples (tested informally for producing correct outputs for created attention mask and when there are no combo rewards)

autodraft/pretrain.py
@@ -1,6 +1,9 @@
 import random
+from collections import namedtuple
+
 import torch
 from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
 
 
 class PretrainDataset(Dataset):
@@ -13,7 +16,8 @@ class PretrainDataset(Dataset):
         return len(self.seed_range)
 
     def __getitem__(self, index):
-        return self.create_example(self.seed_range[index])
+        seed = self.seed_range[index]
+        return self.create_example(seed)
 
     # To create the example input a draft with random rewards is created
     # and random actions are applied up to a team's final action. The
@@ -60,3 +64,63 @@ class PretrainDataset(Dataset):
             action_hat, value_hat = optimal_team_B_final_action(draft)
 
         return *draft.make_nn_input(), action_hat, value_hat
+
+
+PretrainBatch = namedtuple(
+    'PretrainBatch',
+    [
+        'states',       # size [batch_size, state_dim]
+        'role_rs',      # size [batch_size, num_role_rs, role_r_dim]
+        'combo_rs',     # size [batch_size, num_combo_rs, combo_r_dim]
+        'action_hats',  # size [batch_size]
+        'value_hats',   # size [batch_size]
+        'padding_mask', # size [batch_size, 1 + num_role_rs + num_combo_rs]
+    ],
+)
+
+
+def pretrain_collate(batch):
+    batch_states = []
+    batch_role_rs = []
+    batch_combo_rs = []
+    batch_action_hats = []
+    batch_value_hats = []
+
+    for example in batch:
+        state, role_rs, combo_rs, action_hat, value_hat = example
+        batch_states.append(torch.from_numpy(state))
+        batch_role_rs.append(torch.from_numpy(role_rs))
+        batch_combo_rs.append(torch.from_numpy(combo_rs))
+        batch_action_hats.append(action_hat)
+        batch_value_hats.append(value_hat)
+
+    # The draft state, role rewards and combo rewards will be embedded
+    # separately before being concatenated together to form the entire
+    # sequence the transformer model will receive. They must therefore
+    # be padded separately.
+    states = torch.stack(batch_states)
+    role_rs = pad_sequence(batch_role_rs, batch_first=True)
+    combo_rs = pad_sequence(batch_combo_rs, batch_first=True)
+
+    action_hats = torch.tensor(batch_action_hats)
+    value_hats = torch.tensor(batch_value_hats)
+
+    # The fact that role and combo rewards got padded separately must
+    # be taken into account when creating the attention mask for the
+    # final sequence. The final sequence will consist of the draft
+    # state embedding, followed by the role rewards and then the combo
+    # rewards. Here, 1 is for elements that can be attended to and 0
+    # if they are to be ignored.
+    state_mask = torch.ones((len(batch), 1))
+    attended_role_rs = [torch.ones(len(rs)) for rs in batch_role_rs]
+    role_rs_mask = pad_sequence(attended_role_rs, batch_first=True)
+    attended_combo_rs = [torch.ones(len(rs)) for rs in batch_combo_rs]
+    combo_rs_mask = pad_sequence(attended_combo_rs, batch_first=True)
+    padding_mask = torch.cat((state_mask, role_rs_mask, combo_rs_mask), dim=1)
+
+    return PretrainBatch(states,
+                         role_rs,
+                         combo_rs,
+                         action_hats,
+                         value_hats,
+                         padding_mask)