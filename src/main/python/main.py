58d61513d9f98c77214d8784f70774ab918a9665
AlessandroPalmarini <alessandro.palmarini1@gmail.com>
2021-08-27 16:18:42 +0100

    Initial synergy reward model with sort and filter
    Going to continue with the now more specific parts of the synergy model,
    but it may be best to refactor common code into a base reward model
    class.

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,11 +1,11 @@
 from fbs_runtime.application_context.PySide6 import ApplicationContext
-from PySide6.QtWidgets import QMainWindow, QTableView, QWidget, QVBoxLayout, QLineEdit, QPushButton, QAbstractItemView
+from PySide6.QtWidgets import QMainWindow, QTableView, QWidget, QVBoxLayout, QLineEdit, QPushButton, QAbstractItemView, QGridLayout
 from PySide6.QtCore import Slot, QItemSelectionModel
 
 import sys
 import random
 
-from rewards import RoleRewardsModel
+from rewards import RoleRewardsModel, SynergyRewardsModel
 
 
 # @Temp heroes while I focus on building main structure. Will need a
@@ -22,33 +22,44 @@ class TestWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        # role rewards 
         self.role_model = RoleRewardsModel(all_heroes, 'FNC', 'G2')
         self.role_view = QTableView()
         self.role_view.setModel(self.role_model)
         self.role_view.setSelectionMode(QAbstractItemView.ExtendedSelection)
         self.role_view.setSelectionBehavior(QAbstractItemView.SelectRows)
 
+        # synergy rewards
+        self.synergy_model = SynergyRewardsModel('FNC', 'G2')
+        self.synergy_view = QTableView()
+        self.synergy_view.setModel(self.synergy_model)
+        self.synergy_view.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        self.synergy_view.setSelectionBehavior(QAbstractItemView.SelectRows)
+
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
-        self.main_layout = QVBoxLayout(self.central_widget)
-        self.main_layout.addWidget(self.role_view)
+        self.main_layout = QGridLayout(self.central_widget)
+        self.main_layout.addWidget(self.role_view, 0, 0)
+        self.main_layout.addWidget(self.synergy_view, 0, 1)
 
         # test sorting
         self.role_view.setSortingEnabled(True)
+        self.synergy_view.setSortingEnabled(True)
 
         # test filtering
         self.filter_bar = QLineEdit()
-        self.main_layout.addWidget(self.filter_bar)
+        self.main_layout.addWidget(self.filter_bar, 1, 0, 1, 2)
         self.filter_bar.textChanged.connect(self.role_model.filter_rewards)
+        self.filter_bar.textChanged.connect(self.synergy_model.filter_rewards)
 
         # test adding
         self.add_button = QPushButton("add random reward")
-        self.main_layout.addWidget(self.add_button)
+        self.main_layout.addWidget(self.add_button, 2, 0)
         self.add_button.clicked.connect(self.add_random_reward)
 
         # test deleting
         self.delete_button = QPushButton("delete selected rewards")
-        self.main_layout.addWidget(self.delete_button)
+        self.main_layout.addWidget(self.delete_button, 3, 0, 1, 2)
         self.delete_button.clicked.connect(self.delete)
 
     @Slot()
--- src/main/python/rewards.py
+++ src/main/python/rewards.py
@@ -1,4 +1,5 @@
 from operator import itemgetter
+from collections import namedtuple
 
 from PySide6.QtCore import Qt, QAbstractTableModel, QModelIndex
 
@@ -28,7 +29,7 @@ class RoleRewardsModel(QAbstractTableModel):
     def __init__(self, all_heroes, team_1_tag, team_2_tag):
         super().__init__()
 
-        self.data_fields = ("Hero", "Role", team_1_tag, team_2_tag)
+        self.headers = ("Hero", "Role", team_1_tag, team_2_tag)
         self.hero_roles = {hero: set() for hero in all_heroes}
         self.rewards = []
         self.view_rewards = []
@@ -39,7 +40,7 @@ class RoleRewardsModel(QAbstractTableModel):
         if role != Qt.DisplayRole:
             return
         if orientation == Qt.Horizontal:
-            return self.data_fields[section]
+            return self.headers[section]
         else:
             return str(section + 1)
 
@@ -47,7 +48,7 @@ class RoleRewardsModel(QAbstractTableModel):
         return len(self.view_rewards) if not parent.isValid() else 0
 
     def columnCount(self, parent=QModelIndex()):
-        return len(self.data_fields) if not parent.isValid() else 0
+        return len(self.headers) if not parent.isValid() else 0
 
     def data(self, index, role=Qt.DisplayRole):
         reward = self.view_rewards[index.row()]
@@ -123,4 +124,106 @@ class RoleRewardsModel(QAbstractTableModel):
 
         self.layoutAboutToBeChanged.emit()
         self.view_rewards = [r for r in self.rewards if self.contains_filter(r)]
+        self.layoutChanged.emit()
+
+
+# A synergy consists of a group of heroes each with a list of applicable
+# roles they could be in.
+SynergyHero = namedtuple('SynergyHero', ['name', 'roles'])
+
+
+class SynergyReward:
+
+    def __init__(self, heroes, team_1_value, team_2_value):
+        self.heroes = heroes
+        self.team_1_value = team_1_value
+        self.team_2_value = team_2_value
+
+    # convient way to get data for each column in table
+    def __getitem__(self, index):
+        if index < len(self.heroes):
+            return self.heroes[index].name
+        elif index < 5:
+            return None
+        elif index == 5:
+            return self.team_1_value
+        elif index == 6:
+            return self.team_2_value
+        else:
+            raise IndexError
+
+
+class SynergyRewardsModel(QAbstractTableModel):
+
+    def __init__(self, team_1_tag, team_2_tag):
+        super().__init__()
+
+        self.headers = ("Heroes", team_1_tag, team_2_tag)
+        self.all_combos = set()
+        self.rewards = []
+        self.view_rewards = []
+        self.current_sort = (None, None)
+        self.current_filter = ""
+
+    def headerData(self, section, orientation, role):
+        if role != Qt.DisplayRole:
+            return
+        if orientation == Qt.Horizontal:
+            if section < 5: return  # first 5 are all for synergy heroes
+            tag = section - 4
+            return self.headers[tag]
+        else:
+            return str(section + 1)
+
+    def rowCount(self, parent=QModelIndex()):
+        return len(self.view_rewards) if not parent.isValid() else 0
+
+    def columnCount(self, parent=QModelIndex()):
+        return 7 if not parent.isValid() else 0  # 5 for possible heroes and 2 for each tag
+
+    def data(self, index, role=Qt.DisplayRole):
+        reward = self.view_rewards[index.row()]
+        if role == Qt.DisplayRole:
+            return reward[index.column()]
+
+    def sort(self, column, order=Qt.AscendingOrder):
+        # only allow sorting of team values
+        if column < 5: return 
+        self.current_sort = (column, order)
+        if order == Qt.AscendingOrder:
+            self.rewards.sort(key=itemgetter(column))
+        else:
+            self.rewards.sort(key=itemgetter(column), reverse=True)
+        self.layoutAboutToBeChanged.emit()
+        if not self.current_filter:
+            self.view_rewards = list(self.rewards)
+        else:
+            self.view_rewards = [r for r in self.rewards if self.contains_filter(r)]
+        self.layoutChanged.emit()
+
+    # Give users ability to enter multiple hero names separated with a
+    # space. For a reward to contain the filter all parts must be part
+    # of any hero name.
+    def contains_filter(self, reward):
+        filters = self.current_filter.split()
+
+        def filter_in_any_name(text):
+            for hero in reward.heroes:
+                if text in hero.name.lower():
+                    return True
+            return False
+
+        return all(map(filter_in_any_name, filters))
+
+    def filter_rewards(self, text):
+        prev_filter = self.current_filter
+        self.current_filter = text.lower()
+        self.layoutAboutToBeChanged.emit()
+        if not self.current_filter:
+            self.view_rewards = list(self.rewards)
+        elif self.current_filter.startswith(prev_filter):
+            # new view rewards will be subset of old
+            self.view_rewards = [r for r in self.view_rewards if self.contains_filter(r)]
+        else:
+            self.view_rewards = [r for r in self.rewards if self.contains_filter(r)]
         self.layoutChanged.emit()
