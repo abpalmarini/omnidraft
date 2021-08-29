189c538d651cca316b8bb201945d381202ae2bb5
AlessandroPalmarini <alessandro.palmarini1@gmail.com>
2021-08-29 11:26:28 +0100

    refactor to include base class for reward models

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,11 +1,11 @@
 from fbs_runtime.application_context.PySide6 import ApplicationContext
 from PySide6.QtWidgets import QMainWindow, QTableView, QWidget, QVBoxLayout, QLineEdit, QPushButton, QAbstractItemView, QGridLayout
-from PySide6.QtCore import Slot, QItemSelectionModel
+from PySide6.QtCore import Slot, QItemSelectionModel, Qt
 
 import sys
 import random
 
-from rewards import RoleRewardsModel, SynergyRewardsModel
+from rewards import *
 
 
 # @Temp heroes while I focus on building main structure. Will need a
@@ -16,6 +16,7 @@ all_heroes = [
     'Aphelios', 'Ashe', 'AurelionSol', 'Azir',
     'Bard', 'Blitzcrank', 'Brand', 'Braum',
 ]
+all_roles = ('Top', 'Jungle', 'Mid', 'Support', 'Bot')
 
 
 class TestWindow(QMainWindow):
@@ -43,7 +44,10 @@ class TestWindow(QMainWindow):
         self.main_layout.addWidget(self.synergy_view, 0, 1)
 
         # test sorting
+        # @Important to do initial sort first so that a current sort gets set
+        self.role_view.sortByColumn(2, Qt.DescendingOrder)
         self.role_view.setSortingEnabled(True)
+        self.synergy_view.sortByColumn(5, Qt.DescendingOrder)
         self.synergy_view.setSortingEnabled(True)
 
         # test filtering
@@ -53,9 +57,12 @@ class TestWindow(QMainWindow):
         self.filter_bar.textChanged.connect(self.synergy_model.filter_rewards)
 
         # test adding
-        self.add_button = QPushButton("add random reward")
-        self.main_layout.addWidget(self.add_button, 2, 0)
-        self.add_button.clicked.connect(self.add_random_reward)
+        self.add_role_button = QPushButton("add random role reward")
+        self.main_layout.addWidget(self.add_role_button, 2, 0)
+        self.add_role_button.clicked.connect(self.add_role_reward)
+        self.add_synergy_button = QPushButton("add random synergy reward")
+        self.main_layout.addWidget(self.add_synergy_button, 2, 1)
+        self.add_synergy_button.clicked.connect(self.add_synergy_reward)
 
         # test deleting
         self.delete_button = QPushButton("delete selected rewards")
@@ -63,19 +70,39 @@ class TestWindow(QMainWindow):
         self.delete_button.clicked.connect(self.delete)
 
     @Slot()
-    def add_random_reward(self):
-        self.role_model.add_reward(
+    def add_role_reward(self):
+        role_reward = RoleReward(
             random.choice(all_heroes),
-            random.choice(('Top', 'Jungle', 'Mid', 'Support', 'Bot')),
+            random.choice(all_roles),
             random.randrange(0, 1000) / 100,
             random.randrange(0, 1000) / 100,
         )
+        self.role_model.add_reward(role_reward)
+
+    @Slot()
+    def add_synergy_reward(self):
+        num_heroes = random.randint(2, 5)
+        names = random.sample(all_heroes, num_heroes)
+        # just give each one a single role to avoid clashes for now
+        roles = random.sample(all_roles, num_heroes)
+        heroes = {name: role for name, role in zip(names, roles)}
+        synergy_reward = SynergyReward(
+            heroes,
+            random.randrange(0, 1000) / 100,
+            random.randrange(0, 1000) / 100,
+        )
+        self.synergy_model.add_reward(synergy_reward)
 
     @Slot()
     def delete(self):
-        selection_model = self.role_view.selectionModel()
-        indexes = selection_model.selectedRows()
-        self.role_model.delete_rewards(indexes)
+        views = (self.role_view, self.synergy_view)
+        models = (self.role_model, self.synergy_model)
+        for view, model in zip(views, models):
+            selection_model = view.selectionModel()
+            indexes = selection_model.selectedRows()
+            if indexes:
+                model.delete_rewards(indexes)
+            view.clearSelection()
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
--- src/main/python/rewards.py
+++ src/main/python/rewards.py
@@ -4,34 +4,12 @@ import itertools
 
 from PySide6.QtCore import Qt, QAbstractTableModel, QModelIndex
 
-class RoleReward:
 
-    def __init__(self, name, role, team_1_value, team_2_value):
-        self.name = name
-        self.role = role
-        self.team_1_value = team_1_value
-        self.team_2_value = team_2_value
+class BaseRewardsModel(QAbstractTableModel):
 
-    def __getitem__(self, index):
-        if index == 0:
-            return self.name
-        elif index == 1:
-            return self.role
-        elif index == 2:
-            return self.team_1_value
-        elif index == 3:
-            return self.team_2_value
-        else:
-            raise IndexError
-
-
-class RoleRewardsModel(QAbstractTableModel):
-
-    def __init__(self, all_heroes, team_1_tag, team_2_tag):
+    def __init__(self):
         super().__init__()
 
-        self.headers = ("Hero", "Role", team_1_tag, team_2_tag)
-        self.hero_roles = {hero: set() for hero in all_heroes}
         self.rewards = []
         self.view_rewards = []
         self.current_sort = (None, None)
@@ -71,9 +49,6 @@ class RoleRewardsModel(QAbstractTableModel):
             self.view_rewards = [r for r in self.rewards if self.contains_filter(r)]
         self.layoutChanged.emit()
 
-    def contains_filter(self, reward):
-        return self.current_filter in reward.name.lower()
-
     def filter_rewards(self, text):
         prev_filter = self.current_filter
         self.current_filter = text.lower()
@@ -87,9 +62,9 @@ class RoleRewardsModel(QAbstractTableModel):
             self.view_rewards = [r for r in self.rewards if self.contains_filter(r)]
         self.layoutChanged.emit()
 
-    def add_reward(self, name, role, team_1_value, team_2_value):
-        reward = RoleReward(name, role, team_1_value, team_2_value)
-        self.hero_roles[name].add(role)
+    def add_reward(self, reward):
+        # hook for specific reward types to update helper data structures
+        self.update_extra_state(reward, add=True)
 
         # place in correct sorted position
         sort_column, sort_order = self.current_sort
@@ -118,7 +93,7 @@ class RoleRewardsModel(QAbstractTableModel):
         rewards = []
         for reward in self.rewards:
             if reward in del_rewards:
-                self.hero_roles[reward.name].remove(reward.role)
+                self.update_extra_state(reward, add=False)
             else:
                 rewards.append(reward)
         self.rewards = rewards
@@ -126,6 +101,45 @@ class RoleRewardsModel(QAbstractTableModel):
         self.layoutAboutToBeChanged.emit()
         self.view_rewards = [r for r in self.rewards if self.contains_filter(r)]
         self.layoutChanged.emit()
+
+
+class RoleReward:
+
+    def __init__(self, name, role, team_1_value, team_2_value):
+        self.name = name
+        self.role = role
+        self.team_1_value = team_1_value
+        self.team_2_value = team_2_value
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.name
+        elif index == 1:
+            return self.role
+        elif index == 2:
+            return self.team_1_value
+        elif index == 3:
+            return self.team_2_value
+        else:
+            raise IndexError
+
+
+class RoleRewardsModel(BaseRewardsModel):
+
+    def __init__(self, all_heroes, team_1_tag, team_2_tag):
+        super().__init__()
+
+        self.headers = ("Hero", "Role", team_1_tag, team_2_tag)
+        self.hero_roles = {hero: set() for hero in all_heroes}
+
+    def contains_filter(self, reward):
+        return self.current_filter in reward.name.lower()
+
+    def update_extra_state(self, reward, add=True):
+        if add:
+            self.hero_roles[reward.name].add(reward.role)
+        else:
+            self.hero_roles[reward.name].remove(reward.role)
 
 
 class SynergyReward:
@@ -168,53 +182,13 @@ class SynergyReward:
             raise IndexError
 
 
-class SynergyRewardsModel(QAbstractTableModel):
+class SynergyRewardsModel(BaseRewardsModel):
 
     def __init__(self, team_1_tag, team_2_tag):
         super().__init__()
 
-        self.headers = ("Heroes", team_1_tag, team_2_tag)
-        self.hero_role_synergies = set()
-        self.rewards = []
-        self.view_rewards = []
-        self.current_sort = (None, None)
-        self.current_filter = ""
-
-    def headerData(self, section, orientation, role):
-        if role != Qt.DisplayRole:
-            return
-        if orientation == Qt.Horizontal:
-            if section < 5: return  # first 5 are all for synergy heroes
-            tag = section - 4
-            return self.headers[tag]
-        else:
-            return str(section + 1)
-
-    def rowCount(self, parent=QModelIndex()):
-        return len(self.view_rewards) if not parent.isValid() else 0
-
-    def columnCount(self, parent=QModelIndex()):
-        return 7 if not parent.isValid() else 0  # 5 for possible heroes and 2 for each tag
-
-    def data(self, index, role=Qt.DisplayRole):
-        reward = self.view_rewards[index.row()]
-        if role == Qt.DisplayRole:
-            return reward[index.column()]
-
-    def sort(self, column, order=Qt.AscendingOrder):
-        # only allow sorting of team values
-        if column < 5: return 
-        self.current_sort = (column, order)
-        if order == Qt.AscendingOrder:
-            self.rewards.sort(key=itemgetter(column))
-        else:
-            self.rewards.sort(key=itemgetter(column), reverse=True)
-        self.layoutAboutToBeChanged.emit()
-        if not self.current_filter:
-            self.view_rewards = list(self.rewards)
-        else:
-            self.view_rewards = [r for r in self.rewards if self.contains_filter(r)]
-        self.layoutChanged.emit()
+        self.headers = tuple(None for _ in range(5)) + (team_1_tag, team_2_tag)
+        self.hero_role_combos = set()
 
     # Give users ability to enter multiple hero names separated with a
     # space. For a reward to contain the filter all parts must be part
@@ -230,18 +204,16 @@ class SynergyRewardsModel(QAbstractTableModel):
 
         return all(map(filter_in_any_name, filters))
 
-    def filter_rewards(self, text):
-        prev_filter = self.current_filter
-        self.current_filter = text.lower()
-        self.layoutAboutToBeChanged.emit()
-        if not self.current_filter:
-            self.view_rewards = list(self.rewards)
-        elif self.current_filter.startswith(prev_filter):
-            # new view rewards will be subset of old
-            self.view_rewards = [r for r in self.view_rewards if self.contains_filter(r)]
+    def sort(self, column, order=Qt.AscendingOrder):
+        # only allow sorting of team values
+        if column >= 5:
+            BaseRewardsModel.sort(self, column, order)
+
+    def update_extra_state(self, reward, add=True):
+        if add:
+            self.hero_role_combos |= reward.hero_role_asgmts
         else:
-            self.view_rewards = [r for r in self.rewards if self.contains_filter(r)]
-        self.layoutChanged.emit()
+            self.hero_role_combos -= reward.hero_role_asgmts
 
     # Returns a list of all synergy rewards using a role reward.
     def uses_role_reward(self, name, role):
