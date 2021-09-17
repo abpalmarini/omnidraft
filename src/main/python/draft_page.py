18b00d13fba5c8ad83de130d4e4f8295e8a9cc48
AlessandroPalmarini <alessandro.palmarini1@gmail.com>
2021-09-17 10:32:06 +0100

    add rewards page along with dummy draft page into tab widget

---
+++ src/main/python/draft_page.py
@@ -0,0 +1,11 @@
+from PySide6.QtWidgets import QWidget
+
+from ai.draft_ai import DraftAI, A, B, PICK, BAN
+
+
+class DraftPage(QWidget):
+
+    def __init__(self, hero_icons, draft_format, team_tags):
+        super().__init__()
+        
+        pass
--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,14 +1,16 @@
 from fbs_runtime.application_context.PySide6 import ApplicationContext
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QTabWidget
 from PySide6.QtGui import QIcon
 
 import sys
 
 from rewards_page import RewardsPage
+from draft_page import DraftPage
+from ai.draft_ai import A, B, PICK, BAN
 
 
-# @Temp heroes while I focus on building main structure. Will need a
-# way to select game and load in all heroes @Later.
+# @Temp heroes and draft format while I focus on building the main
+# structure. Need a way to select game and load in all heroes @Later.
 all_heroes = [
     'Aatrox', 'Ahri', 'Akali', 'Akshan',
     'Alistar', 'Amumu', 'Anivia', 'Annie',
@@ -17,16 +19,39 @@ all_heroes = [
 ]
 all_roles = ("Top Laner", "Jungler", "Mid Laner", "Bot Laner", "Support")
 team_tags = ('FNC', 'G2')
+draft_format = [
+    (A, BAN),
+    (B, BAN),
+    (A, BAN),
+    (B, BAN),
+    (A, PICK),
+    (B, PICK),
+    (B, PICK),
+    (A, PICK),
+    (A, PICK),
+    (B, PICK),
+    (B, PICK),
+    (A, PICK),
+    (A, PICK),
+    (B, PICK),
+]
 
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
 
+    # icons
     hero_icons = {h: QIcon(appctxt.get_resource(h + '.png')) for h in all_heroes}
     role_icons = {r: QIcon(appctxt.get_resource(r + '.png')) for r in all_roles}
     arrow_icon = QIcon(appctxt.get_resource("right-arrow.png"))
+
+    # pages
+    tab_widget = QTabWidget()
+    tab_widget.addTab(RewardsPage(hero_icons, role_icons, arrow_icon, team_tags), "Rewards")
+    tab_widget.addTab(DraftPage(hero_icons, draft_format, team_tags), "Draft")
+
     window = QMainWindow()
-    window.setCentralWidget(RewardsPage(hero_icons, role_icons, arrow_icon, team_tags))
+    window.setCentralWidget(tab_widget)
     window.resize(1600, 800)
     window.show()
 
