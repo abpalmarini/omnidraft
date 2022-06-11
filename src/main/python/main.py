ca81607631baf0bc2274e9d691d38280f57d1fb5
AlessandroPalmarini <alessandro.palmarini1@gmail.com>
2022-06-11 11:48:01 +0100

    more heroes and dummy rewards for testing other features (will delete later)

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -6,6 +6,7 @@ from PySide6.QtGui import QIcon
 import sys
 
 from rewards_page import RewardsPage
+from reward_models import RoleReward
 from draft_page import DraftPage
 from ai.draft_ai import A, B, PICK, BAN
 
@@ -13,10 +14,15 @@ from ai.draft_ai import A, B, PICK, BAN
 # @Temp heroes and draft format while I focus on building the main
 # structure. Need a way to select game and load in all heroes @Later.
 all_heroes = [
-    'Aatrox', 'Ahri', 'Akali', 'Akshan',
-    'Alistar', 'Amumu', 'Anivia', 'Annie',
-    'Aphelios', 'Ashe', 'AurelionSol', 'Azir',
-    'Bard', 'Blitzcrank', 'Brand', 'Braum',
+    "Aatrox", "Ahri", "Akali", "Akshan",
+    "Alistar", "Amumu", "Anivia", "Annie",
+    "Aphelios", "Ashe", "AurelionSol", "Azir",
+    "Bard", "Blitzcrank", "Brand", "Braum",
+    "Caitlyn", "Camille", "Cassiopeia", "Corki",
+    "Darius", "Diana", "DrMundo", "Draven",
+    "Ekko", "Elise", "Evelynn", "Ezreal", 
+    "Fiddlesticks", "Fiora", "Fizz", "Galio",
+    "Gangplank", "Garen",
 ]
 all_roles = ("Top Laner", "Jungler", "Mid Laner", "Bot Laner", "Support")
 team_tags = ('FNC', 'G2')
@@ -52,6 +58,7 @@ class TabWidget(QTabWidget):
         super().__init__()
 
         self.rewards_page = rewards_page
+        self.add_test_rewards()
         self.draft_page = draft_page
         self.addTab(rewards_page, "Rewards")
         self.addTab(draft_page, "Draft")
@@ -63,6 +70,31 @@ class TabWidget(QTabWidget):
         if index == 1:
             rewards = self.rewards_page.get_rewards()
             self.draft_page.set_rewards(*rewards)
+
+    # @Temp method to add in some test rewards to help with implementing
+    # other features.
+    def add_test_rewards(self):
+        role_model = self.rewards_page.reward_types[0].model
+        role_model.add_reward(RoleReward(all_heroes[0], all_roles[0], 10, 10))
+        role_model.add_reward(RoleReward(all_heroes[1], all_roles[1], 10, 0))
+        role_model.add_reward(RoleReward(all_heroes[2], all_roles[2], 0, 10))
+        role_model.add_reward(RoleReward(all_heroes[3], all_roles[3], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[4], all_roles[4], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[5], all_roles[0], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[6], all_roles[1], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[7], all_roles[2], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[8], all_roles[3], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[9], all_roles[4], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[10], all_roles[0], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[11], all_roles[1], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[12], all_roles[2], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[13], all_roles[3], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[14], all_roles[4], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[15], all_roles[0], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[16], all_roles[1], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[17], all_roles[2], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[18], all_roles[3], 1, 1))
+        role_model.add_reward(RoleReward(all_heroes[19], all_roles[4], 1, 1))
 
 
 if __name__ == '__main__':
