37494269cc2d2145bd242a7c6c1a5ea4bc7a776d
AlessandroPalmarini <alessandro.palmarini1@gmail.com>
2022-06-24 11:59:45 +0100

    create a new reward set entry with initial data, and load data up upone initialising object with valid name

--- src/main/python/reward_set.py
+++ src/main/python/reward_set.py
@@ -1,10 +1,26 @@
+import pickle
+import re
 from pathlib import Path
 
 
 class RewardSet:
+    """
+    Class with functionality for the creation, editing and saving of reward
+    sets along with any cached transposition tables.
+    """
 
-    def __init__():
-        pass
+    data_filename = "data.p"
+
+    def __init__(self, name):
+        """
+        Load the reward set with the given name to allow for access to, and 
+        editing of, its data.
+        """
+        self.path = self.reward_sets_dir() / name
+        if not self.path.is_dir():
+            raise ValueError(f"No reward set called '{name}' exists.")
+        with open(self.path / self.data_filename, 'rb') as data_file:
+            self.data = pickle.load(data_file)
 
     @staticmethod
     def reward_sets_dir():
@@ -20,3 +36,29 @@ class RewardSet:
     def all_reward_sets(cls):
         """Return a list of all the saved reward sets name/dirname."""
         return [p.name for p in cls.reward_sets_dir().iterdir() if p.is_dir()]
+
+    @classmethod
+    def new_reward_set(cls, name, team_tags, draft_format):
+        """
+        Create a new reward set entry with the given name, data and an empty
+        set of rewards. The method creates a folder, saves the initial data
+        and returns a RewardSet object for the newly created reward set.
+        """
+        if not re.match("^[A-Za-z0-9 _-]*$", name):  # ensure name can be used as a dirname
+            raise ValueError("Name contains invalid characters.")
+
+        path = cls.reward_sets_dir() / name
+        path.mkdir()
+        reward_set_data = {
+            "name": name,
+            "team_tags": team_tags,
+            "draft_format": draft_format,
+            "role_rs": [],
+            "synergy_rs": [],
+            "counter_rs": [],
+            "team_1_A_tt": None,
+            "team_2_A_tt": None,
+        }
+        with open(path / cls.data_filename, 'wb') as data_file:
+            pickle.dump(reward_set_data, data_file)
+        return RewardSet(name)
